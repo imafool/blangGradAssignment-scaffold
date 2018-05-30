38f1cbb9edf0f1f0ed768d1c526fd700e6eb35fc
Kevin Chern <kevinn.chern@gmail.com>
2018-05-30 13:39:30 -0700

    fix blang no sudo needed

--- nextflow/work/9c/361f65af661ea25a30926755ceaf85/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-set -e
-java -cp `cat classpath` -Xmx8g matchings.PermutedClustering     --experimentConfigs.managedExecutionFolder false     --experimentConfigs.saveStandardStreams false     --experimentConfigs.recordExecutionInfo false     --experimentConfigs.recordGitInfo false     --model.nGroups 20     --model.groupSize 5     --engine Forward
-mv samples generated
--- nextflow/work/f8/7c167a3210630c1337ff96fd0bea17/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-echo 'scriptName: permuted-clustering.nf' >> pipeline-info.txt
-echo 'start: Mon May 28 15:45:38 PDT 2018' >> pipeline-info.txt
-echo 'runName: distraught_curran' >> pipeline-info.txt
-echo 'nextflow.version: 0.29.1' >> pipeline-info.txt
