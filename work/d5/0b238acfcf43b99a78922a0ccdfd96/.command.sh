681d001f72d9461bf14de353b223c5b42f8c6824
Kevin Chern <kevinn.chern@gmail.com>
2018-06-08 10:14:35 -0700

    cleaned repo

--- work/d5/0b238acfcf43b99a78922a0ccdfd96/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-echo 'scriptName: permuted-clustering.nf' >> pipeline-info.txt
-echo 'start: Thu Jun 07 18:31:32 PDT 2018' >> pipeline-info.txt
-echo 'runName: ecstatic_poisson' >> pipeline-info.txt
-echo 'nextflow.version: 0.29.1' >> pipeline-info.txt
